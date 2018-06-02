2c39235b645f5b070288da722508c2f7d6322ea4
Shravani PL <pl.shravani@gmail.com>
2018-06-02 19:30:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ return "Welcome, " +name + ". You are number " + line + " in line"
 
 function nowServing(katzDeliLine) {
   
-  var str = katzDeliLine.slice(1);
+  var str = katzDeliLine.slice(0,1);
   return "Currently serving " + str + "."
   if(katzDeliLine.length === 0) {
     return "There is nobody waiting to be served!";
