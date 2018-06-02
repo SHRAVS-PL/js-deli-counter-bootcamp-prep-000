4a8b37414ed7647d6c216ff3ed54afaad6bca14a
Shravani PL <pl.shravani@gmail.com>
2018-06-02 20:04:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ function nowServing(katzDeliLine) {
    if(katzDeliLine.length === 0) {
     return "There is nobody waiting to be served!";
   }
-  var str = katzDeliLine.slice(0,1);
   return "Currently serving " + str + "."
  
 }
