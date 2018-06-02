dd301827e593f40a442fd532eba6f15e9bed2639
Shravani PL <pl.shravani@gmail.com>
2018-06-02 19:25:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function currentLine(katzDeliLine) {
    current.push(i+1 +". " +katzDeliLine[i]);
    
  }
-    return "The line is currently " +current;
+    return "The line is currently: " +current;
 
 }
 }
